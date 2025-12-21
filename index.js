27e1dfa44852a70ad00b7ec04450f6abdf1eb671
Fazeelit <fazeelahmadsstit@gmail.com>
2025-12-21 09:44:24 +0500

    ok

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000", // local frontend
   "http://0.0.0.0:8080",
-  "https://muhafizoprojectit.vercel.app"
+  "https://mediposfrontendproject.vercel.app/"
 ];
 
 app.use(cors({
