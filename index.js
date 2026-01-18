a69c63977c9afc68ed73496648c9aa9003355b29
Fazeel Ahmad <108638723+Fazeelit@users.noreply.github.com>
2026-01-18 13:14:23 +0500

    ok

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ dbConnect();
 const app = express();
 
 // ------------------ CORS ------------------
-const allowedOrigins = ["http://localhost:3000","https://mediposbackend.onrender.com/api"];
+const allowedOrigins = ["http://localhost:3000"];
 
 app.use(
   cors({
